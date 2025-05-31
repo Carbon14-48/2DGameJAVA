648b89316e2c2437c9e30518421266067b35dbc0
Carbon14-48 <mohammedberrichi4@gmail.com>
2025-05-31 15:52:02 +0100

    part 2 : done

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -4,6 +4,7 @@ import javax.swing.JPanel;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 public class GamePanel extends JPanel implements Runnable{
@@ -15,7 +16,12 @@ public class GamePanel extends JPanel implements Runnable{
     final int maxScreenRow=12;
     final int screenWidth = maxScreenCol*tileSize;//768 px
     final int screenHeight = maxScreenRow*tileSize;//576 px
+
+    //fps
     final int FPS=60;
+    int frameCount = 0;
+    long lastTime = System.currentTimeMillis();
+    int currentFPS = 0;
     //poistion du joueur initiale 
     int playerX = 100;
     int playerY=100;
@@ -47,7 +53,12 @@ public class GamePanel extends JPanel implements Runnable{
                 System.out.println("current Time"+currentTime);  
                 update();
                 repaint();
-                
+                frameCount++;
+        if (System.currentTimeMillis() - lastTime >= 1000) {
+            currentFPS = frameCount;
+            frameCount = 0;
+            lastTime = System.currentTimeMillis();
+        }
                 try{
                     double remainingTime =nextDrawTime-System.nanoTime();
                     remainingTime=remainingTime/1000000;
@@ -78,6 +89,10 @@ public class GamePanel extends JPanel implements Runnable{
             Graphics2D g2 = (Graphics2D) g;
             g2.setColor(Color.WHITE);
             g2.fillRect(playerX, playerY, tileSize, tileSize);
+             // Draw FPS
+            g2.setColor(Color.YELLOW);
+            g2.setFont(new Font("Arial", Font.BOLD, 20));
+            g2.drawString("FPS: " + currentFPS, 10, 20);
             g2.dispose();
         }
 }
