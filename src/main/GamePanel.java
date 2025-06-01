a62670eb120aa497d50a5958d3057bd261709012
Carbon14-48 <mohammedberrichi4@gmail.com>
2025-06-01 14:45:03 +0100

    Basic map

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,6 +3,7 @@ package main;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 import java.awt.Color;
 import java.awt.Dimension;
@@ -14,21 +15,18 @@ public class GamePanel extends JPanel implements Runnable{
     final int scale = 3;
 
   public  final int tileSize = originalTileSize*scale;//48x48
-    final int maxScreenCol=16;
-    final int maxScreenRow=12;
-    final int screenWidth = maxScreenCol*tileSize;//768 px
-    final int screenHeight = maxScreenRow*tileSize;//576 px
+  public  final int maxScreenCol=16;
+     public final int maxScreenRow=12;
+     public final int screenWidth = maxScreenCol*tileSize;//768 px
+     public final int screenHeight = maxScreenRow*tileSize;//576 px
 
     //fps
     final int FPS=60;
     int frameCount = 0;
     long lastTime = System.currentTimeMillis();
     int currentFPS = 0;
-    //poistion du joueur initiale 
-    int playerX = 100;
-    int playerY=100;
-    int playerSpeed =4;
-
+   
+TileManager tileM = new TileManager(this);
 
     KeyHandler keyH = new KeyHandler();
     Player player = new Player(this, keyH);
@@ -83,6 +81,8 @@ public class GamePanel extends JPanel implements Runnable{
 
             super.paintComponent(g);
             Graphics2D g2 = (Graphics2D) g;
+            
+            tileM.draw(g2);
             player.draw(g2);
              // Draw FPS
             g2.setColor(Color.YELLOW);
