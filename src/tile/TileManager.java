b1c8e8ef4c3673b1e1ebcaee57de1bbf6e7b5178
Carbon14-48 <mohammedberrichi4@gmail.com>
2025-06-07 13:04:24 +0100

    lot of problems to fix

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -93,6 +93,9 @@ public class TileManager {
         setup(39, "earth", false);
         setup(40, "wall", true);
         setup(41, "tree", true);
+        setup(42, "hut", false);
+        setup(43, "floor01", false);
+        setup(44, "table01", false);
 
 
        
